10d3809ceac9c14dc1f25a3abc0d6b1492f0b4ca
Lanre14 <olanrewajuilyas0@gmail.com>
2023-10-16 11:12:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+function updateScore(team, points) {
+    if (team === 1) {
+        var scoreElement = document.getElementById('team1_score');
+    } else if (team === 2) {
+        var scoreElement = document.getElementById('team2_score');
+    }
+
+    var currentScore = parseInt(scoreElement.innerText);
+    scoreElement.innerText = currentScore + points;
+}
+let reload = document.querySelector('.steve')
+reload.addEventListener('click', ()=> {
+    window.location.reload()
+})
